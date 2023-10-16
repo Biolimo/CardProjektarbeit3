d45a242ea7989455e5ba39772ba244e0e7d4fa85
Biolo <maxim1neumann@gmail.com>
2023-10-16 15:28:27 +0200

    fixed method to checkUserAnswer

--- Card/src/main/java/com/example/Card/Cards/KinderKarten/MuSeCard.java
+++ Card/src/main/java/com/example/Card/Cards/KinderKarten/MuSeCard.java
@@ -24,21 +24,13 @@ public class MuSeCard extends Card {
         System.out.println("userAnswers = " + userAnswers[0]);
         System.out.println("userAnswers = " + userAnswers[1]);
 
-        if(areAllNumbersInArray(userAnswers, muSeCard.getCorrectAnswers())) muSeCard.setSuccessCounter(getSuccessCounter()+1);
+        if(areAllNumbersInArray(muSeCard.getCorrectAnswers(), userAnswers)) muSeCard.setSuccessCounter(getSuccessCounter()+1);
         return areAllNumbersInArray(userAnswers, muSeCard.getCorrectAnswers());
     }
 
     public static boolean areAllNumbersInArray(int[] array1, int[] array2) {
-        // Convert array2 to a List for easy lookup
-        Integer[] array2Integer = Arrays.stream(array2).boxed().toArray(Integer[]::new);
 
-        // Check if all numbers from array1 are in array2
-        for (int num : array1) {
-            if (!Arrays.asList(array2Integer).contains(num)) {
-                return false; // If a number from array1 is not in array2, return false
-            }
-        }
-        return true; // All numbers from array1 are in array2
+        return Arrays.equals(array1, array2);
     }
 
 }
